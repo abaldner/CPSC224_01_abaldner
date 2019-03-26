e6138183e7b9b7b1e1d6904c702cfdf9d0584f7f
abaldner <46766106+abaldner@users.noreply.github.com>
2019-03-25 22:10:25 -0700

    Add files via upload

--- MotionParallax.java
+++ MotionParallax.java
@@ -69,6 +69,20 @@ class MotionPanel extends JPanel implements ActionListener {
 	private int leavesY = 70;
 	private int leavesWidth;
 	private int leavesHeight;
+        
+        private int arcX = 5;
+        private int arcY = 100;
+        private int arcWidth;
+        private int arcHeight;
+        private int startAngle;
+        private int arcAngle;
+        
+        private int arcX2 = 50;
+        private int arcY2 = 350;
+        private int arcWidth2;
+        private int arcHeight2;
+        private int startAngle2;
+        private int arcAngle2;
 	
 	
 
@@ -90,11 +104,26 @@ class MotionPanel extends JPanel implements ActionListener {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g); // call superclass's paintComponent
 		
+                //paint all the graphics
 		skyWidth = 1000;
 		skyHeight = 1000;
 		g.setColor(Color.cyan);
 		g.fillRect(skyX, skyY, skyWidth, skyHeight);
 		
+                g.setColor(Color.black);
+                arcWidth = 400;
+                arcHeight = 500;
+                startAngle = 30;
+                arcAngle = 130;
+                g.drawArc(arcX, arcY, arcWidth, arcHeight, startAngle, arcAngle);
+                
+                g.setColor(Color.GREEN);
+                arcWidth2 = 200;
+                arcHeight2 = 300;
+                startAngle2 = 30;
+                arcAngle2 = 130;
+                g.drawArc(arcX2, arcY2, arcWidth2, arcHeight2, startAngle2, arcAngle2);
+                
 		oceanWidth = 30;
 		oceanHeight = 40;
 		g.setColor(Color.blue);
@@ -122,26 +151,16 @@ class MotionPanel extends JPanel implements ActionListener {
 		stumpHeight = 30;
 		g.fillRect(stumpX, stumpY, stumpWidth, stumpHeight);
 		
-		//g.setColor(Color.green);
+		
 		g.setColor(Color.GREEN);
 		leavesWidth = 50;
-		leavesHeight = 100;
+		leavesHeight = 80;
 		g.fillRect(leavesX, leavesY, leavesWidth, leavesHeight);
 		
+                
 		
 		
-		/*	g.setColor(Color.GRAY);
-		Mountain1Width = 100;
-		Mountain1Height = 150;
-		g.fillPolygon(Mountain1X, Mountain1Y, 200);
-*/
-		//g.fillPolygon(Mountain1X,  Mountain1Y,  Mountain1Width);
-		/*
-		g.setColor(Color.GRAY);
-		Mountain2Width = 100;
-		Mountain2Height = 200;
-		g.fillRect(Mountain2X,  Mountain2Y,  Mountain2Width, Mountain2Height);*/
-	
+		
 	}
 
 	public void paintSun(Graphics g) {
@@ -186,16 +205,23 @@ class MotionPanel extends JPanel implements ActionListener {
 		}
 
 		public void mouseMoved(MouseEvent e) {
+                    //new values made depending on where the mouse is
 			int mouseX = e.getX();
 			int mouseY = e.getY();
 			
 			skyX = mouseX - 300;
 			skyY = mouseY - 300;
+                        
+                        arcX = 40 + ((mouseX-300) /15);
+                        arcY = 50 + (mouseY /6);
+                        
+                        arcX2 = 50 + (mouseX -500) /15;
+                        arcY2 = 170 + (mouseY /15);
 			
 			oceanX = (mouseX + 5);
 			oceanY = (mouseY + 5);
-			// System.out.println(oceanX);
-			// mouseX += oceanX;
+			
+                       
 			sunX = 190 + (mouseX - 75) / 12;
 			sunY = 10 + (mouseY - 75) / 12;
 			
@@ -206,7 +232,7 @@ class MotionPanel extends JPanel implements ActionListener {
 			stumpY = 170 + (mouseY) / 12;
                         
                         leavesX = 25 + (mouseX - 300)/12;
-                        leavesY = 70 + (mouseY) /12;
+                        leavesY = 95 + (mouseY) /12;
 		}
 		
 
